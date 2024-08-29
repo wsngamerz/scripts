cd0f14c8f3e30264b35d98f659f336e4c1c286d3
William Neild <wneild5@gmail.com>
2024-08-29 14:40:06 +0100

    add spawn4x4 script

---
+++ spawn4x4.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Not sure why the extra 300px requirement on my system but oh well
+gnome-terminal --geometry 94x22+0+300
+gnome-terminal --geometry 94x22+960+300
+gnome-terminal --geometry 94x22+0+840
+gnome-terminal --geometry 94x22+960+840
